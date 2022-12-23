fef87eaf3187cf2dd42d6010ebe2035f96240bdd
Anirudh Sugavi <38699686+anirudhsugavi@users.noreply.github.com>
2022-12-23 20:43:04 +0530

    configured property loader

---
+++ accounts/src/util/properties-loader.ts
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+const CONFIG_HOME = '../../config';
+const ENV = process.env.NODE_ENV ?? 'prod';
+
+// env files are in root/config/
+const getAbsolutePath = (relative: string): string => {
+  return path.resolve(__dirname, relative);
+};
+
+if (ENV.toLowerCase() !== 'prod') {
+  const configFile = `${CONFIG_HOME}/.${ENV.toLowerCase()}.env`;
+  dotenv.config({ path: getAbsolutePath(configFile) });
+} else {
+  dotenv.config({ path: getAbsolutePath(`${CONFIG_HOME}/.env`) });
+}
+
+export default {
+  APP_PORT: process.env.APP_PORT,
+  MONGO_URL: process.env.MONGO_URL ?? '',
+  MONGO_USERNAME: process.env.MONGO_USERNAME,
+  MONGO_PASSWORD: process.env.MONGO_PASSWORD,
+  MONGO_DB_NAME: process.env.MONGO_DB_NAME,
+  MONGO_DB_AUTH_SOURCE: process.env.MONGO_DB_AUTH_SOURCE,
+};
