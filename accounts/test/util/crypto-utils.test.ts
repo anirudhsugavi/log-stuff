d33581131e150d79e914e55e6dec500a2d361a7c
Anirudh Sugavi <38699686+anirudhsugavi@users.noreply.github.com>
2023-01-02 17:09:50 +0530

    added more test

--- accounts/test/util/crypto-utils.test.ts
+++ accounts/test/util/crypto-utils.test.ts
@@ -1,4 +1,5 @@
 import { hashPassword, comparePassword } from '../../src/util/crypto-utils';
+import { BadRequestError } from '../../src/util/app-errors';
 
 const TEST_PLAIN = 'test1234';
 
@@ -6,5 +7,17 @@ describe('Hash password validation', () => {
   it('valid hashing', async () => {
     const hashed = await hashPassword(TEST_PLAIN);
     expect(comparePassword(TEST_PLAIN, hashed)).toBeTruthy();
+  });
+
+  it.each([
+    '', '  ']
+  )('when plain password is "%s"', async (password) => {
+    try {
+      await hashPassword(password);
+    } catch (err) {
+      if (err instanceof BadRequestError) {
+        expect(err.message).toEqual('argument is empty');
+      }
+    }
   });
 });
