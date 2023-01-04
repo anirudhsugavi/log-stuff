11f93a543811de5d46e2f4d4365a6992dd4a9d24
Anirudh Sugavi <38699686+anirudhsugavi@users.noreply.github.com>
2023-01-04 14:58:30 +0530

    added more tests for crypto

--- accounts/test/util/crypto-utils.test.js
+++ accounts/test/util/crypto-utils.test.js
@@ -4,20 +4,25 @@ import { BadRequestError } from '../../src/util/app-errors';
 const TEST_PLAIN = 'test1234';
 
 describe('Hash password validation', () => {
-  it('valid hashing', async () => {
+  it('valid password', async () => {
     const hashed = await hashPassword(TEST_PLAIN);
-    expect(comparePassword(TEST_PLAIN, hashed)).toBeTruthy();
+    expect(await comparePassword(TEST_PLAIN, hashed)).tobe(true);
+  });
+
+  it('invalid password', async () => {
+    const hashed = await hashPassword(TEST_PLAIN);
+    // const result = await comparePassword('incorrect', hashed);
+    // expect(result).toBeFalsy();
+    expect(await comparePassword('incorrect', hashed)).tobe(false);
   });
 
   it.each([
-    '', '  ']
-  )('when plain password is "%s"', async (password) => {
-    try {
-      await hashPassword(password);
-    } catch (err) {
-      if (err instanceof BadRequestError) {
-        expect(err.message).toEqual('argument is empty');
-      }
-    }
+    '', '  ', undefined, null,
+  ])('when plain password is "%s"', async (password) => {
+    await expect(hashPassword(password)).rejects.toThrowError(BadRequestError);
+  });
+
+  it('no password', async () => {
+    await expect(hashPassword()).rejects.toThrowError(BadRequestError);
   });
 });
