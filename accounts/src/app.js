b5afa557e03cffee20cb2239786d54b639b8da13
Anirudh Sugavi <38699686+anirudhsugavi@users.noreply.github.com>
2023-01-08 21:44:01 +0530

    reordered things

--- accounts/src/app.js
+++ accounts/src/app.js
@@ -7,28 +7,21 @@ const props = require('./util/properties-loader');
 const logger = require('./util/logger');
 const { errorHandler, errorLogger } = require('./middleware/error-handler');
 
-const exitApp = () => {
-  disconnect()
-    .then(() => {
-      logger.debug('Disconnected from DB');
-      process.exit();
-    })
-    .catch((err) => {
-      logger.error('Error closing db connection', err);
-      process.exit();
-    });
-};
-
 connect()
   .then(() => {
     const app = express();
 
     app.use(express.json());
     app.use(cors());
+
+    // routers
     app.use('/auth', authRouter);
     app.use(userRouter);
+
+    // error handlers
     app.use(errorLogger);
     app.use(errorHandler);
+
     app.listen(props.APP_PORT, () => {
       logger.info(`Listening on port ${props.APP_PORT}`);
     }).on('error', (err) => {
@@ -43,5 +36,17 @@ connect()
     logger.error('Failed to start Auth Server.', err);
     exitApp();
   });
+
+function exitApp() {
+  disconnect()
+    .then(() => {
+      logger.debug('Disconnected from DB');
+      process.exit();
+    })
+    .catch((err) => {
+      logger.error('Error closing db connection', err);
+      process.exit();
+    });
+}
 
 process.on('SIGINT', exitApp);
