07de512de012df15985d40299014f21992aaaf45
Anirudh Sugavi <38699686+anirudhsugavi@users.noreply.github.com>
2023-05-03 17:57:57 +0530

    most basic chat stuff

--- chat/client/src/components/chat.js
+++ chat/client/src/components/chat.js
@@ -1,37 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
 // import Bubble from './bubble'
 
 function Chat() {
-    let idConst = 1
 
-    const [messages, setMessages] = useState([])
-    const [text, setText] = useState('');
-    const sendMessage = (e) => {
-        e.preventDefault()
-        setMessages([...messages, { id: idConst++, texts: text }]);
-    }
+  useEffect(() => {
+    connectToServer();
+  });
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log(e.target.to.value, e.target.message.value);
+    displayMessage(e.target.message.value);
+    e.target.message.value = '';
+  };
+
+  const displayMessage = (message) => {
+    const container = document.querySelector('.message-container');
+    const bubble = document.createElement('div');
+    bubble.textContent = message;
+    container.append(bubble);
+    container.scrollTop = container.scrollHeight;
+  };
+
+  const connectToServer = () => {
+    const socket = io('http://localhost:5000');
+  };
 
   return (
     <div>
-    <div className='message-container'>
-        <ul className='message-list'>
-        {messages.map(msg => (
-          <li key={msg.id}>{msg.texts}</li>
-        ))}
-        </ul>
+      <div className='message-container'></div>
+      <form onSubmit={(e) => sendMessage(e)}>
+        <label for="to">To:</label>
+        <input id="to" type="text" />
+        <label for="message">Message:</label>
+        <input id="message" type="text" />
+        <button type="submit">Send</button>
+      </form>
     </div>
-        <form
-         onSubmit={(e) => sendMessage(e)}
-         >
-        To: <input
-         id="to"></input>
-          Message: <input id="message" name="message"
-          onChange={(e) => setText(e.target.value)}></input>
-          <button type='submit'
-        >Send</button>
-        </form>
-    </div>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
